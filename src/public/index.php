40c54074b96972278186967735af3956330b36b0
Joe Molloy <molloyjoe@gmail.com>
2024-12-19 15:52:33 +0000

    Updated index.php file with short description

--- src/public/index.php
+++ src/public/index.php
@@ -1,8 +1,9 @@
 <html>
-  <title>Peter's Docker</title>
+  <title>Docker PHP/Mysql/Nginx Dev Environment</title>
   <head></head>
   <body>
-    <h1>Hello Peter!</h1>
+    <h1 style="text-align:center">It Lives!</h1>
+    <p style="text-align:center">A basic docker dev environment setup for PHP development using Docker featuring PHP-FPM, MariaDB, Nginx and Mailhog.</p>
     <?php phpinfo();?>
   </body>
 </html>
